0ba9eaeed94cc5a1ea6352205501054020f8838d
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-07-14 11:24:16 +0530

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,11 +11,20 @@ import {
 
 const Router = express.Router();
 
+// user routes
 Router.post("/register", register);
-Router.patch("/:id", authenticate, authorize("admin"), userUpdate);
-Router.delete("/:id", authenticate, authorize("admin"), userDelete);
-Router.get("/users", authenticate, authorize("admin"), showUsers);
 Router.post("/login", login);
 Router.post("/profile", showProfile)
+// === get - profile
+// === patch - update profile
+
+// admin routes
+Router.patch("/:id", authenticate, authorize("admin"), userUpdate);
+Router.delete("/:id", authenticate, authorize("admin"), userDelete);
+// Router.get("/users", authenticate, authorize("admin"), showUsers);
+Router.get("/users", showUsers);
+// === get - getUser
+// === post - adduser
+
 
 export default Router;
